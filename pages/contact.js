b10ffb36b3dcf6ea3fe802562236b22e9e4d1222
jasminetam <jasminetam94@gmail.com>
2022-08-19 23:31:47 +0100

    fixing bugs

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,10 @@
-import Header from "../Components/Common/Header";
-import Navigation from "../Components/Common/Navigation";
+import React from 'react';
+import Header from '../Components/Common/Header';
+import Navigation from '../Components/Common/Navigation';
 import CustomHead from '../Components/Common/CustomHead';
-import ContactForm from "../Components/ContactForm/ContactForm";
-import Footer from "../Components/Common/Footer";
-import SidebarIcon from "../Components/Common/SidebarIcon";
+import ContactForm from '../Components/ContactForm/ContactForm';
+import Footer from '../Components/Common/Footer';
+import SidebarIcon from '../Components/Common/SidebarIcon';
 
 export default function Contact() {
   const topRef = React.useRef(null);
@@ -20,14 +21,14 @@ export default function Contact() {
         <title>Contact | Jasmine&apos;s Blog</title>
       </CustomHead>
       <main className="center">
-      <Header />
+        <Header />
         <Navigation topRef={topRef} executeScroll={executeScroll} />
-      <div className="projects">
-        <ContactForm />
-        <SidebarIcon />
-      </div>
+        <div className="projects">
+          <ContactForm />
+          <SidebarIcon />
+        </div>
         <Footer />
-        </main>
+      </main>
     </div>
   );
 }
